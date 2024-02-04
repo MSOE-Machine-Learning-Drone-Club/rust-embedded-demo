521b807420b9e171fbcfcd0efce1bed58b4bdb3b
Braden-Everson <eversonb@msoe.edu>
2024-02-04 08:42:50 -0600

    Add camera

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use esp_idf_svc::hal::gpio::PinDriver;
 use esp_idf_svc::hal::peripherals::Peripherals;
 use esp_idf_triton::network::network::Network;
 use esp_idf_hal::gpio::*;
+use esp_camera_rs::Camera;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -14,7 +15,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    let peripherals = Peripherals::take()?;
+    /*let peripherals = Peripherals::take()?;
     let mut led = PinDriver::output(peripherals.pins.gpio12)?;
 
     let mut button1 = PinDriver::input(peripherals.pins.gpio4)?;
@@ -23,8 +24,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     button1.set_pull(Pull::Down)?;
     button2.set_pull(Pull::Down)?;
 
-    let model_str = "D|3|2|10.845654 11.002682 -13.501029 -14.699452 -53.440483 -53.715294|
-    -6.101849 49.06853 61.28852#D|1|3|30.350481 -78.40228 70.861206|-19.532055#D|1|1|15.161753|-3.7315714".to_string();
+    let model_str = "D|3|2|10.845654 11.002682 -13.501029 -14.699452 -53.440483 -53.715294|-6.101849 49.06853 61.28852#D|1|3|30.350481 -78.40228 70.861206|-19.532055#D|1|1|15.161753|-3.7315714".to_string();
 
     let mut xor_net = Network::deserialize_triton_fmt_string(model_str);
 
@@ -52,5 +52,56 @@ fn main() -> Result<(), Box<dyn Error>> {
         } else {
             led.set_low()?;
         }
+    }*/
+    let camera_config = esp_idf_sys::camera::camera_config_t {
+        pin_pwdn: 32,
+        pin_reset: -1,
+        pin_xclk: 0,
+        sccb_i2c_port: -1,
+        __bindgen_anon_1:
+            esp_idf_sys::camera::camera_config_t__bindgen_ty_1 {
+                pin_sccb_sda: 26
+            },
+        __bindgen_anon_2:
+            esp_idf_sys::camera::camera_config_t__bindgen_ty_2 {
+                pin_sscb_scl: 27,
+            },
+        pin_d7: 35,
+        pin_d6: 34,
+        pin_d5: 39,
+        pin_d4: 36,
+        pin_d3: 21,
+        pin_d2: 19,
+        pin_d1: 18,
+        pin_d0: 5,
+        pin_vsync: 25,
+        pin_href: 23,
+        pin_pclk: 22,
+        xclk_freq_hz: 20000000,
+        ledc_timer: esp_idf_sys::ledc_timer_t_LEDC_TIMER_0,
+        ledc_channel: esp_idf_sys::ledc_channel_t_LEDC_CHANNEL_0,
+        pixel_format: esp_idf_sys::cam::pixformat_t_PIXFORMAT_JPEG,
+        frame_size: esp_idf_sys::cam::framesize_t_FRAMESIZE_QVGA,
+        jpeg_quality: 12,
+        fb_count: 1,
+        fb_location:
+            esp_idf_sys::cam::camera_fb_location_t_CAMERA_FB_IN_PSRAM,
+        grab_mode:
+            esp_idf_sys::cam::camera_grab_mode_t_CAMERA_GRAB_WHEN_EMPTY,
+    };
+    unsafe {
+        if esp_idf_sys::camera::esp_camera_init(&camera_config) != 0 {
+            println!("camera init failed!");
+            return Ok(());
+        } else {
+            println!("camera ready!");
+        }
+        let fb = esp_idf_sys::camera::esp_camera_fb_get();
+        println!("Picture taken! Its size was: {} bytes", unsafe {
+            (*fb).len
+        });
+        let data = std::slice::from_raw_parts((*fb).buf, (*fb).len);
+        println!("{data:?}");
     }
+    Ok(())
 }
